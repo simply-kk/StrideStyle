812b51e7b4c84e66142f3c60ab0322306aeab8a5
Khalid Khan <shhkhan33@gmail.com>
2024-02-28 21:47:53 +0530

    React Basic for practice.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navigation from "./components/Navigation";
+import "./App.css";
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <Hero />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <img src="/images/logo.png" alt="logo" />
+      </div>
+      <ul>
+        <li href="#">Menu</li>
+        <li href="#">Location</li>
+        <li href="#">About</li>
+        <li href="#">Contact</li>
+      </ul>
+      <button>Login</button>
+    </nav>
+  );
+};
+
+export default Navigation;
